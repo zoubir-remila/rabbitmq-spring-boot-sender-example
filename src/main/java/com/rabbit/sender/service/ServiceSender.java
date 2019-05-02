4d980470b0ee51b8b9a43775a4c5ce68c72bcdcb
REMILA <zoubir.remila@gmail.com>
2019-05-02 03:25:43 +0200

    init commit

---
+++ src/main/java/com/rabbit/sender/service/ServiceSender.java
@@ -0,0 +1,35 @@
+package com.rabbit.sender.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbit.sender.dto.UserDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceSender {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServiceSender.class);
+    @Autowired
+    private Queue queue;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    public void sendSimpleMessage() {
+        UserDto user = new UserDto("JHON", "DOE", 99);
+        try {
+            rabbitTemplate.convertAndSend(queue.getName(), objectMapper.writeValueAsString(user));
+        } catch (JsonProcessingException e) {
+            LOGGER.error("An Error Occurred the Process");
+        }
+
+    }
+}
