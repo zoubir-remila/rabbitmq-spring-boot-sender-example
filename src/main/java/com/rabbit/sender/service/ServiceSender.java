0ba8bea2a11202e68385d1739020c2883b60a3fa
REMILA <zoubir.remila@gmail.com>
2019-05-02 13:40:43 +0200

    make it pretty

--- src/main/java/com/rabbit/sender/service/ServiceSender.java
+++ src/main/java/com/rabbit/sender/service/ServiceSender.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class ServiceSender {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ServiceSender.class);
+
     @Autowired
     private Queue queue;
 
@@ -26,6 +27,12 @@ public class ServiceSender {
     public void sendSimpleMessage() {
         UserDto user = new UserDto("JHON", "DOE", 99);
         try {
+            LOGGER.info("----------------------------------------------");
+            LOGGER.info("******** Sending message to the queue ********");
+            LOGGER.info("|          firstName : {}                   |", user.getFirstName());
+            LOGGER.info("|          lastName : {}                     |", user.getLastName());
+            LOGGER.info("|              age : {}                       |", user.getAge());
+            LOGGER.info("----------------------------------------------");
             rabbitTemplate.convertAndSend(queue.getName(), objectMapper.writeValueAsString(user));
         } catch (JsonProcessingException e) {
             LOGGER.error("An Error Occurred the Process");
